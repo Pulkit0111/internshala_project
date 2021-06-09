2326b28bd07ad62ec27fd40398e5d39563530815
Deependra-Negi <916deepaknegi@gmail.com>
2021-06-09 18:39:07 +0530

    updated the employer login page

--- resouces/login_page_student/resouces/js/script.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById("registerbtn");
-btn.addEventListener('mouseover', changeIcon);
-btn.addEventListener('mouseout', DownIcon)
-function changeIcon() {
-    btn.innerHTML = `Login/Register  <i class="is_icon_header ic-24-filled-up-arrow"></i>`
-}
-function DownIcon() {
-    btn.innerHTML = `Login/Register  <i class="is_icon_header ic-24-filled-down-arrow"></i>`
-}
